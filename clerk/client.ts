0779e85872205ff20f0d489bda3f225be6c0a025
Indrajit <43838718+thisisindrajit@users.noreply.github.com>
2024-07-22 10:51:09 +0530

    fixed small bug

--- clerk/client.ts
+++ clerk/client.ts
@@ -1,5 +1,5 @@
 import { createClerkClient } from '@clerk/nextjs/server';
 
-const createdClerkClient: ClerkClient = createClerkClient({ secretKey: process.env.CLERK_SECRET_KEY });
+const createdClerkClient = createClerkClient({ secretKey: process.env.CLERK_SECRET_KEY });
 
 export default createdClerkClient;
